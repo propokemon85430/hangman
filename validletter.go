478c18c6633f75335ef8d022e31b5b977fe71eb3
homerofsteel <40791058+propokemon85430@users.noreply.github.com>
2024-11-05 22:53:07 +0100

    Update test validletter.go

--- validletter.go
+++ validletter.go
@@ -5,52 +5,18 @@ import (
 	"strings"
 )
 
-func ValidLetter() string {
-	tab := CreateWord()
-	vies := 10
-	final_word := tab[0]
-	hidden_word := tab[1]
-	empty_word := ""
-	fmt.Println("Choose:")
-	fmt.Println(final_word)
-	final_word = final_word[:len(final_word)-1]
-	hidden_word = hidden_word[:len(hidden_word)-1]
-	fmt.Println("You have to guess the word starting with this: ", hidden_word)
-	var letter string
-	for vies != 0 {
-		fmt.Scanln(&letter)
-		if strings.Contains(final_word, letter) {
-			println("The letter chosen is in the word")
-			for i := 0; i < len([]rune(final_word)); i++ {
-				if string(final_word[i]) == letter {
-					empty_word = empty_word + string(final_word[i])
-				} else if hidden_word[i] >= 'a' && hidden_word[i] <= 'z' {
-					empty_word = empty_word + string(final_word[i])
-				} else {
-					empty_word = empty_word + "_"
-				}
+// ValidLetter vérifie si la lettre proposée est dans le mot final
+func ValidLetter(finalWord, hiddenWord, letter string) (string, int) {
+	if strings.Contains(finalWord, letter) {
+		fmt.Println("La lettre choisie est dans le mot")
+		for i := 0; i < len([]rune(finalWord)); i++ {
+			if string(finalWord[i]) == letter {
+				hiddenWord = strings.Replace(hiddenWord, ".", string(finalWord[i]), 1) // Remplace le premier point par la lettre
 			}
-			if empty_word == final_word {
-				fmt.Println("Congrats, you found the word which is", final_word)
-				break
-			}
-		} else {
-			println("Not present in the word, _ attempts remaining")
-			vies = vies - 1
 		}
-		if len([]rune(empty_word)) > len([]rune(final_word)) {
-			empty_word = empty_word[:len(empty_word)-1]
-		}
-		if len([]rune(empty_word)) != 0 {
-			fmt.Println(empty_word)
-			hidden_word = empty_word
-			empty_word = ""
-		}
-		if vies == 0 {
-			fmt.Println("Vous avez perdu. Le mot était :", final_word)
-			return "perdu"
-		}
-
+		return hiddenWord, 0 // Retourner le mot caché mis à jour
 	}
-	return ("perdu")
+
+	fmt.Println("La lettre n'est pas présente dans le mot")
+	return hiddenWord, 1 // Retourner le mot caché sans modification
 }
