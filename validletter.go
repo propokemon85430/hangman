99b9c89e33813cba61e57f80d72089278d6a66a3
homerofsteel <40791058+propokemon85430@users.noreply.github.com>
2024-11-05 23:09:19 +0100

    Update validletter.go

--- validletter.go
+++ validletter.go
@@ -1,22 +1,24 @@
 package pendu
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 )
 
 // ValidLetter vérifie si la lettre proposée est dans le mot final
 func ValidLetter(finalWord, hiddenWord, letter string) (string, int) {
-	if strings.Contains(finalWord, letter) {
-		fmt.Println("La lettre choisie est dans le mot")
-		for i := 0; i < len([]rune(finalWord)); i++ {
-			if string(finalWord[i]) == letter {
-				hiddenWord = strings.Replace(hiddenWord, ".", string(finalWord[i]), 1) // Remplace le premier point par la lettre
-			}
-		}
-		return hiddenWord, 0 // Retourner le mot caché mis à jour
-	}
+    // Vérification si la lettre est présente dans le mot final
+    if strings.Contains(finalWord, letter) {
+        fmt.Println("La lettre choisie est dans le mot")
+        // Remplacer le premier point (".") par la lettre dans le mot caché
+        for i := 0; i < len(finalWord); i++ {
+            if string(finalWord[i]) == letter {
+                hiddenWord = strings.Replace(hiddenWord, ".", string(finalWord[i]), 1)
+            }
+        }
+        return hiddenWord, 0 // Retourner le mot caché mis à jour
+    }
 
-	fmt.Println("La lettre n'est pas présente dans le mot")
-	return hiddenWord, 1 // Retourner le mot caché sans modification
+    fmt.Println("La lettre n'est pas présente dans le mot")
+    return hiddenWord, 1 // Retourner le mot caché sans modification
 }
