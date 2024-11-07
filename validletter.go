a3c9d5e2bbc975409556665d6065807dd1095dae
homerofsteel <40791058+propokemon85430@users.noreply.github.com>
2024-11-07 16:03:25 +0100

    Update validletter.go

--- validletter.go
+++ validletter.go
@@ -5,20 +5,21 @@ import (
     "strings"
 )
 
-// ValidLetter vérifie si la lettre proposée est dans le mot final
 func ValidLetter(finalWord, hiddenWord, letter string) (string, int) {
-    // Vérification si la lettre est présente dans le mot final
     if strings.Contains(finalWord, letter) {
         fmt.Println("La lettre choisie est dans le mot")
-        // Remplacer le premier point (".") par la lettre dans le mot caché
+
+        hiddenWordArr := []rune(hiddenWord)
+
         for i := 0; i < len(finalWord); i++ {
-            if string(finalWord[i]) == letter {
-                hiddenWord = strings.Replace(hiddenWord, ".", string(finalWord[i]), 1)
+            if rune(finalWord[i]) == rune(letter[0]) && hiddenWordArr[i] == '.' {
+                hiddenWordArr[i] = rune(letter[0]) 
             }
         }
-        return hiddenWord, 0 // Retourner le mot caché mis à jour
+
+        return string(hiddenWordArr), 0 
     }
 
     fmt.Println("La lettre n'est pas présente dans le mot")
-    return hiddenWord, 1 // Retourner le mot caché sans modification
+    return hiddenWord, 1 
 }
