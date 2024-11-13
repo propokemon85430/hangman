cf64de79eb807dfaa53dd0523cf8d83abf5ca262
homerofsteel <40791058+propokemon85430@users.noreply.github.com>
2024-11-13 19:22:30 +0100

    Update validletter.go

--- validletter.go
+++ validletter.go
@@ -9,16 +9,13 @@ func ValidLetter(finalWord, hiddenWord, letter string) (string, int) {
     if strings.Contains(finalWord, letter) {
         fmt.Println("La lettre choisie est dans le mot")
 
-        hiddenRunes := []rune(hiddenWord)
-        finalRunes := []rune(finalWord)
-
-        for i := 0; i < len(finalRunes); i++ {
-            if finalRunes[i] == rune(letter[0]) && hiddenRunes[i] == '.' {
-                hiddenRunes[i] = rune(letter[0])
+        for i := 0; i < len(finalWord); i++ {
+            if finalWord[i] == letter[0]) && hiddenWord[i] == '.' {
+                hiddenWord[i] = letter[0]
             }
         }
 
-        return string(hiddenRunes), 0
+        return hiddenWord, 0
     }
 
     fmt.Println("La lettre n'est pas présente dans le mot")
